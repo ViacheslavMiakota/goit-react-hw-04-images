f99ff99f6f4d10e51b4ab8b80dd25b5d59ef0900
ViacheslavMiakota <parazyt2010@gmail.com>
2023-01-02 22:19:15 +0200

    add useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
 import Searchbar from 'components/Searchbar/Searchbar';
@@ -9,90 +9,158 @@ import Loader from 'components/Loader/Loader';
 import Button from 'components/Button/Button';
 import ModalBox from 'components/Modal/Modal';
 
-class App extends React.Component {
-  state = {
-    query: '',
-    hits: [],
-    page: 1,
-    loading: false,
-    totalHits: 0,
-    selectedImage: null,
-    shouModal: false,
-  };
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [hits, setHits] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  async componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    async function fetshBase() {
       try {
-        this.setState({ loading: true });
-        const { query, page } = this.state;
+        setLoading(true);
         const { hits, totalHits } = await fetchResult(query, page);
-
         if (!totalHits) {
           toast.success(`Nothing found for your request :${query}`);
           return;
         }
-
-        this.setState(prevState => ({
-          hits: [...prevState.hits, ...hits],
-          totalHits: totalHits,
-          loading: false,
-        }));
+        setHits(prevHits => [...prevHits, ...hits]);
+        setTotalHits(totalHits);
+        setLoading(false);
       } catch (error) {
         toast.error('Something went wrong : Try reloading the page.');
       } finally {
-        this.setState({ loading: false });
+        setLoading(false);
       }
     }
-  }
+    fetshBase();
+  }, [query, page]);
 
-  handleSubmit = query => {
-    this.setState({
-      query,
-      hits: [],
-      page: 1,
-    });
+  console.log(query.length);
+  const handleSubmit = query => {
+    setQuery(query);
+    setHits([]);
+    setPage(1);
   };
 
-  incrementImage = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const incrementImage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  selectImage = imageURL => {
-    this.setState({ selectedImage: imageURL });
-  };
-  closeImage = () => {
-    this.setState({ selectedImage: null });
-  };
+  const selectImage = imageURL => setSelectedImage(imageURL);
 
-  render() {
-    const { loading, hits, totalHits, selectedImage } = this.state;
-    return (
-      <>
-        <Container>
-          <Searchbar handleSubmit={this.handleSubmit} />
-          {loading && <Loader isLoading={loading} />}
-          {hits.length > 0 && (
-            <ImgGallery selectImage={this.selectImage} hits={hits} />
-          )}
+  const closeImage = () => setSelectedImage(null);
 
-          {Boolean(totalHits) && totalHits !== hits.length && (
-            <Button loadMoreProp={this.incrementImage} />
-          )}
-          {selectedImage !== null && (
-            <ModalBox
-              selectedImage={selectedImage}
-              closeImage={this.closeImage}
-            />
-          )}
-          <Toaster position="top-center" />
-        </Container>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Container>
+        <Searchbar handleSubmit={handleSubmit} />
+        {loading && <Loader isLoading={loading} />}
+        {hits.length > 0 && (
+          <ImgGallery selectImage={selectImage} hits={hits} />
+        )}
+
+        {Boolean(totalHits) && totalHits !== hits.length && (
+          <Button loadMoreProp={incrementImage} />
+        )}
+        {selectedImage !== null && (
+          <ModalBox selectedImage={selectedImage} closeImage={closeImage} />
+        )}
+        <Toaster position="top-center" />
+      </Container>
+    </>
+  );
+};
+
+// class App extends React.Component {
+//   state = {
+//     query: '',
+//     hits: [],
+//     page: 1,
+//     loading: false,
+//     totalHits: 0,
+//     selectedImage: null,
+//     shouModal: false,
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.query !== this.state.query ||
+//       prevState.page !== this.state.page
+//     ) {
+//       try {
+//         this.setState({ loading: true });
+//         const { query, page } = this.state;
+//         const { hits, totalHits } = await fetchResult(query, page);
+
+//         if (!totalHits) {
+//           toast.success(`Nothing found for your request :${query}`);
+//           return;
+//         }
+
+//         this.setState(prevState => ({
+//           hits: [...prevState.hits, ...hits],
+//           totalHits: totalHits,
+//           loading: false,
+//         }));
+//       } catch (error) {
+//         toast.error('Something went wrong : Try reloading the page.');
+//       } finally {
+//         this.setState({ loading: false });
+//       }
+//     }
+//   }
+
+//   handleSubmit = query => {
+//     this.setState({
+//       query,
+//       hits: [],
+//       page: 1,
+//     });
+//   };
+
+//   incrementImage = () => {
+//     this.setState(prevState => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   selectImage = imageURL => {
+//     this.setState({ selectedImage: imageURL });
+//   };
+//   closeImage = () => {
+//     this.setState({ selectedImage: null });
+//   };
+
+//   render() {
+//     const { loading, hits, totalHits, selectedImage } = this.state;
+//     return (
+//       <>
+//         <Container>
+//           <Searchbar handleSubmit={this.handleSubmit} />
+//           {loading && <Loader isLoading={loading} />}
+//           {hits.length > 0 && (
+//             <ImgGallery selectImage={this.selectImage} hits={hits} />
+//           )}
+
+//           {Boolean(totalHits) && totalHits !== hits.length && (
+//             <Button loadMoreProp={this.incrementImage} />
+//           )}
+//           {selectedImage !== null && (
+//             <ModalBox
+//               selectedImage={selectedImage}
+//               closeImage={this.closeImage}
+//             />
+//           )}
+//           <Toaster position="top-center" />
+//         </Container>
+//       </>
+//     );
+//   }
+// }
 export default App;
